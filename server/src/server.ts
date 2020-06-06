582f887ac50de2ccf12144c534924d887c76e305
Filipe Maia <filipebsmaia@gmail.com>
2020-06-06 13:55:26 -0300

    App

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import path from "path";
+
+import { errors } from "celebrate";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
+
+app.listen(3333, () => {
+  console.log("Application started at port 3333 🚀!");
+});
